b513c8c24eac23fab702f839b30008f9e6d169f5
dxncat <xidarkmoon@gmail.com>
2024-06-07 13:32:49 -0500

    java funciones genericas

--- src/main/java/org/generics/adso/java/hashtable/Main.java
+++ src/main/java/org/generics/adso/java/hashtable/Main.java
@@ -1,5 +1,57 @@
 package org.generics.adso.java.hashtable;
 
-public class Main {
+import java.util.List;
 
+//crear la clase hashtable
+class HashItem<K, V> {
+    K key;
+    V value;
+    public HashItem(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "HashItem{" + "key=" + key + ", value=" + value + '}';
+    }
+    
+}
+
+class Customer {
+    String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+    
+}
+
+class Medico {
+    String nombre;
+
+    public Medico(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        HashItem<String, Integer> item = new HashItem<String, Integer>("A", 10);
+        HashItem<Long, Customer> item2 = new HashItem<Long, Customer>(3L, new Customer("Juan"));
+        HashItem<Double, Medico> item3 = new HashItem<Double, Medico>(3.0, new Medico("Juan"));
+        List hashTable = List.of(
+            item,
+            item2,
+            item3
+        );
+
+        for (Object o : hashTable) {
+            System.out.println(o);
+        }
+
+        
+        
+    }
 }
--- src/main/java/org/generics/adso/java/store/Main.java
+++ src/main/java/org/generics/adso/java/store/Main.java
@@ -15,9 +15,21 @@ class Store {
     private Object item;
 }
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+class Customer {
+    String name;
+}
+
 public class Main {
     public static void main(String[] args) {
         Store store = new Store(1);
-        System.out.println(store.getItem());
+        Integer item1 = (Integer) store.getItem();
+        Store store2 = new Store(new Customer("Juan"));
     }
+
+    
 }
